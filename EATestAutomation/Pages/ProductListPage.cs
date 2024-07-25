3720e1c4733298ec2f8b87c644c53eedb1f208af
Divya.Pateliya <Divya.Pateliya@arcurve.com>
2024-07-25 11:42:57 -0600

    progress

---
+++ EATestAutomation/Pages/ProductListPage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Playwright;
+
+namespace EAApplicationTest.Pages
+{
+    public class ProductListPage
+    {
+        private readonly IPage _page;
+
+        public ProductListPage(IPage page)
+        {
+            _page = page;
+        }
+        
+        private ILocator _lnkProductList => _page.GetByRole(AriaRole.Link, new() { Name = "Product" });
+
+        private ILocator _lnkCreate => _page.GetByRole(AriaRole.Link, new() { Name = "Create" });
+
+        public async Task CreateProductAsync()
+        {
+            await _lnkProductList.ClearAsync();
+            await _lnkCreate.ClickAsync();
+        }
+    }
+}
--- EATestAutomation/UnitTest1.cs
+++ EATestAutomation/UnitTest1.cs
@@ -1,6 +1,8 @@
+using EAApplicationTest.Pages;
 using EaFramework.Config;
 using EAFramework.Config;
 using EAFramework.Driver;
+using Microsoft.Playwright;
 
 namespace EAApplicationTest
 {
@@ -37,6 +39,32 @@ namespace EAApplicationTest
             await page.ClickAsync("text=Login");
             await page.GetByLabel("Username").FillAsync("admin");
             await page.GetByLabel("Password").FillAsync("password");
+        }
+
+        [Test]
+        public async Task MyTest()
+        {
+            var page = await _driver.Page;
+            await page.GotoAsync("http://localhost:33084/");
+
+            ProductListPage productListPage = new ProductListPage(page);
+
+            await productListPage.CreateProductAsync();
+
+            await page.GetByLabel("Name").ClickAsync();
+            await page.GetByLabel("Name").FillAsync("Not UPS");
+            await page.GetByLabel("Description").ClickAsync();
+            await page.GetByLabel("Description").FillAsync("Something related to a power supply");
+            await page.Locator("#Price").ClickAsync();
+            await page.Locator("#Price").FillAsync("135");
+            await page.GetByLabel("ProductType").SelectOptionAsync(new[] { "3" });
+            await page.GetByRole(AriaRole.Button, new() { Name = "Create" }).ClickAsync();
+
+            await page.GetByRole(AriaRole.Row, new() { Name = "71 Not UPS Something related" }).GetByRole(AriaRole.Link).First.ClickAsync();
+            await page.GetByRole(AriaRole.Link, new() { Name = "Back to List" }).ClickAsync();
+            await page.GetByRole(AriaRole.Row, new() { Name = "71 Not UPS Something related" }).GetByRole(AriaRole.Link).Nth(1).ClickAsync();
+            await page.GetByRole(AriaRole.Link, new() { Name = "Edit" }).ClickAsync();
+            await page.GetByRole(AriaRole.Button, new() { Name = "Save" }).ClickAsync();
         }
 
         [TearDown]
